ccdba7c24e5f18852426bb05f463ce45fcc488b7
piaand <pia.m.andersin@gmail.com>
2020-05-09 18:51:44 +0300

    alteration button shows now only to authenthicated site owner

---
+++ src/main/java/projekti/ThymeleafConfig.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekti;
+
+/**
+ *
+ * @author piaandersin
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect(){
+        return new SpringSecurityDialect();
+    }
+}
